b562da21609b5f2f6b5b5b9a2a863e6eb3297afc
Luis Gustavo de sousa <dgutarr2@gmail.com>
2024-07-04 12:24:07 -0300

    atualização de caminhos

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/MeuPortfolio/api': {
-        target: 'http://localhost:3000', // Porta do seu servidor backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/MeuPortfolio\/api/, '/api'),
-      },
-    },
-  },
-});
+})
