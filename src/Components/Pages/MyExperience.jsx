2fa988ddd6c56c1bf4c3caa0dee3a4fa87161a5b
Luis Gustavo de sousa <dgutarr2@gmail.com>
2024-07-04 12:07:16 -0300

    atualização dos caminhas das imagens

--- src/Components/Pages/MyExperience.jsx
+++ src/Components/Pages/MyExperience.jsx
@@ -17,9 +17,6 @@ export default function MyExperience() {
         <Typography marginLeft={7} marginRight={35} marginTop={5} sx={{textIndent: '20px', fontSize:'24px'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ipsam sint eligendi sit, nisi incidunt quisquam eveniet 
             aliquam quam aspernatur temporibus asperiores est modi necessitatibus. Perspiciatis illum ratione impedit ipsam.</Typography>
       </Box>
-      <Box>
-        <MyContact/>
-      </Box>
     </Box>
   )
 }
