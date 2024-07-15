d21f8cb7eb1a3e14ac41a376bf00737736bf9b4e
Luis Gustavo de sousa <dgutarr2@gmail.com>
2024-07-15 19:22:09 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MeuPortfolio/', // Caminho base para o GitHub Pages
+  base: '', 
   plugins: [react()],
 });
